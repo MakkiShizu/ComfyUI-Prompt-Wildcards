1a0b67c59f455ee169e583840fe34632d26f4bc7
MakkiShizu <142442886+MakkiShizu@users.noreply.github.com>
2025-01-07 23:35:50 +0800

    add base_path

--- nodes.py
+++ nodes.py
@@ -2,15 +2,27 @@ import os
 import random
 
 from pathlib import Path
+import folder_paths
 
 wildcards_dir = Path(__file__).parent / "wildcards"
 os.makedirs(wildcards_dir, exist_ok=True)
-print(f"Using wildcards dir: {wildcards_dir}")
+wildcards_dirr = Path(folder_paths.base_path) / "wildcards"
+# os.makedirs(wildcards_dirr, exist_ok=True)
+print(f"Using wildcards dir:☯️{wildcards_dir} と {wildcards_dirr}☯️")
 
-WILDCARDS_LIST = ["None"] + [
-    str(wildcard.relative_to(wildcards_dir))[:-4]
-    for wildcard in wildcards_dir.rglob("*.txt")
-]
+full_dirs = [wildcards_dir, wildcards_dirr]
+
+WILDCARDS_LIST = (
+    ["None"]
+    + [
+        "custom_nodes | " + str(wildcard.relative_to(wildcards_dir))[:-4]
+        for wildcard in wildcards_dir.rglob("*.txt")
+    ]
+    + [
+        "base_path | " + str(wildcard.relative_to(wildcards_dirr))[:-4]
+        for wildcard in wildcards_dirr.rglob("*.txt")
+    ]
+)
 
 
 class makiwildcards:
@@ -55,25 +67,40 @@ class makiwildcards:
         ]
         results = []
 
-        for root, dirs, files in os.walk(wildcards_dir):
-            for wildcard in selected_wildcards:
-                wildcard_file = Path(root) / f"{wildcard}.txt"
-                if wildcard_file.is_file():
-                    with open(wildcard_file, "r", encoding="utf-8") as f:
-                        lines = f.readlines()
-                        if lines:
-                            random.seed(seed)
-                            random_line = random.choice(lines).strip()
-                            results.append(random_line)
+        for full_dir in full_dirs:
+            for root, dirs, files in os.walk(full_dir):
+                for wildcard in selected_wildcards:
+                    if wildcard == "None":
+                        continue
+                    else:
+                        if wildcard.startswith("custom_nodes | "):
+                            wildcard_filename = wildcard[len("custom_nodes | ") :]
+                            target_dir = wildcards_dir
+                        if wildcard.startswith("base_path | "):
+                            wildcard_filename = wildcard[len("base_path | ") :]
+                            target_dir = wildcards_dirr
+                        if target_dir:
+                            wildcard_file = (
+                                Path(target_dir) / f"{wildcard_filename}.txt"
+                            )
+                            if wildcard_file.is_file():
+                                with open(wildcard_file, "r", encoding="utf-8") as f:
+                                    lines = f.readlines()
+                                    if lines:
+                                        random.seed(seed)
+                                        random_line = random.choice(lines).strip()
+                                        results.append(random_line)
+                            else:
+                                print(f"Wildcard File not found: {wildcard_file}")
+
+                joined_result = ", ".join(results)
+                print(f"wildcards:{joined_result} ||| seed:{seed}")
+
+                if text == "":
+                    joined_result = f"{joined_result}"
                 else:
-                    print(f"Wildcard File not found: {wildcard_file}")
-
-            joined_result = ", ".join(results)
-            print(f"wildcards:{joined_result} ||| seed:{seed}")
-
-            if text is not None:
-                joined_result = f"{text},{joined_result}"
-            return (joined_result,)
+                    joined_result = f"{text},{joined_result}"
+                return (joined_result,)
 
 
 # textconcatenate is revise from https://github.com/WASasquatch/was-node-suite-comfyui/blob/main/WAS_Node_Suite.py#L10311-L10362 2024/12/07
